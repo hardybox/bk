7042a2496d421e1b834f0bece5f1ca20fe5ff810
hardybox <hardybox@163.com>
2018-08-25 17:59:40 +0800

    new

--- conf/settings_development.py
+++ conf/settings_development.py
@@ -19,7 +19,7 @@ from settings import APP_ID
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',  # 默认用mysql
-        'NAME': 'mydemo1',                        # 数据库名 (默认与APP_ID相同)
+        'NAME': 'mgl-helloword',                        # 数据库名 (默认与APP_ID相同)
         'USER': 'root',                        # 你的数据库user
         'PASSWORD': '123456',                        # 你的数据库password
         'HOST': '127.0.0.1',                   # 开发的时候，使用localhost
